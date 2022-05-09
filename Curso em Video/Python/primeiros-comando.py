2df5890ede03a4afc9331645cef86a402889995c
Diego Andrade <contato.andradediego@gmail.com>
2022-05-09 14:47:07 -0300

    Push de projetos do Curso em Vídeo para nuvem

---
+++ Curso em Video/Python/primeiros-comando.py
@@ -0,0 +1,9 @@
+from tkinter import dialog
+
+
+n1 = input('1: ')
+n2 = input('2: ')
+
+nt = n1+n2
+
+print(nt)
---
+++ Curso em Video/Python/scripts-python/test-01.py
@@ -0,0 +1,14 @@
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+print(nome, idade, peso)
+
+nome = input('Qual o seu nome?')
+print('Boas-vindas', nome, '!!!')
+
+
+dia = input('Insira o dia de seu nascimento')
+mes = input('Insira o mês de seu nascimento')
+ano = input('Insira o ano de seu nascimento')
+
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano)
